c5499cb874d9a3c213d3d9f075c6d4aa658e2eb2
ram-janapala <rjanapala@commure.com>
2025-01-21 21:37:43 +0530

    Adding param suupport for variables

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -5,6 +5,7 @@ from business_rules.variables import BaseVariables, string_rule_variable, numeri
 from business_rules.fields import FIELD_TEXT, FIELD_NUMERIC, FIELD_SELECT
 from unittest import TestCase
 
+
 class SomeVariables(BaseVariables):
 
     @string_rule_variable()
@@ -19,6 +20,14 @@ class SomeVariables(BaseVariables):
     def true_bool(self):
         return True
 
+    @numeric_rule_variable(
+        params=[{"fieldType": FIELD_NUMERIC, "name": "x", "label": "X"},
+                {"fieldType": FIELD_NUMERIC, "name": "y", "label": "Y"}]
+    )
+    def addition(self, x, y):
+        return x + y
+
+
 class SomeActions(BaseActions):
 
     @rule_action(params={"foo": FIELD_NUMERIC})
@@ -31,15 +40,16 @@ class SomeActions(BaseActions):
                           'name': 'baz',
                           'label': 'Baz',
                           'options': [
-                            {'label': 'Chose Me', 'name': 'chose_me'},
-                            {'label': 'Or Me', 'name': 'or_me'}
-                        ]}])
+                              {'label': 'Chose Me', 'name': 'chose_me'},
+                              {'label': 'Or Me', 'name': 'or_me'}
+                          ]}])
     def some_select_action(self, baz): pass
 
 
 class IntegrationTests(TestCase):
     """ Integration test, using the library like a user would.
     """
+
     def test_true_boolean_variable(self):
         condition = {
             'name': 'true_bool',
@@ -85,6 +95,26 @@ class IntegrationTests(TestCase):
         with self.assertRaises(AssertionError):
             check_condition(condition, SomeVariables())
 
+    def test_numeric_variable_with_params(self):
+        condition = {
+            "name": "addition",
+            "operator": "equal_to",
+            "value": 15,
+            "params": {"x": 9, "y": 6},
+        }
+        condition_result = check_condition(condition, SomeVariables())
+        self.assertTrue(condition_result)
+
+    def test_check_missing_params(self):
+        condition = {
+            "name": "addition",
+            "operator": "equal_to",
+            "value": 10,
+            "params": {},
+        }
+        err_string = "addition\(\) missing 2 required positional arguments: 'x' and 'y'"
+        with self.assertRaisesRegex(TypeError, err_string):
+            check_condition(condition, SomeVariables())
 
     def test_export_rule_data(self):
         """ Tests that export_rule_data has the three expected keys
@@ -92,93 +122,112 @@ class IntegrationTests(TestCase):
         """
         all_data = export_rule_data(SomeVariables(), SomeActions())
         self.assertEqual(all_data.get("actions"),
-                [{"name": "some_action",
-                  "label": "Some Action",
-                  "docs": None,
-                  "params": [{'fieldType': 'numeric', 'label': 'Foo', 'name': 'foo'}]},
-                 {"name": "some_other_action",
-                  "label": "woohoo",
-                  "docs": None,
-                  "params": [{'fieldType': 'text', 'label': 'Bar', 'name': 'bar'}]},
-                 {"name": "some_select_action",
-                  "label": "Some Select Action",
-                  "docs": None,
-                  "params":[{'fieldType': FIELD_SELECT,
-                             'name': 'baz',
-                             'label': 'Baz',
-                             'options': [
-                                {'label': 'Chose Me', 'name': 'chose_me'},
-                                {'label': 'Or Me', 'name': 'or_me'}
-                            ]}]
-                  }
-                 ])
-
+                         [{"name": "some_action",
+                           "label": "Some Action",
+                           "docs": None,
+                           "params": [{'fieldType': 'numeric', 'label': 'Foo', 'name': 'foo'}]},
+                          {"name": "some_other_action",
+                           "label": "woohoo",
+                           "docs": None,
+                           "params": [{'fieldType': 'text', 'label': 'Bar', 'name': 'bar'}]},
+                          {"name": "some_select_action",
+                           "label": "Some Select Action",
+                           "docs": None,
+                           "params": [{'fieldType': FIELD_SELECT,
+                                       'name': 'baz',
+                                       'label': 'Baz',
+                                       'options': [
+                                           {'label': 'Chose Me', 'name': 'chose_me'},
+                                           {'label': 'Or Me', 'name': 'or_me'}
+                                       ]}]
+                           }
+                          ])
+        print(all_data.get("variables"))
         self.assertEqual(all_data.get("variables"),
-                         [{"name": "foo",
+                         [{"name": "addition",
+                            "label": "Addition",
+                            "field_type": "numeric",
+                            "options": [],
+                            "docs": None,
+                            "params": [{'fieldType': 'numeric', 'name': 'x', 'label': 'X'},
+                                       {'fieldType': 'numeric', 'name': 'y', 'label': 'Y'}
+                                       ]
+                          },
+                         {"name": "foo",
                            "label": "Foo",
                            "docs": None,
                            "field_type": "string",
-                           "options": []},
+                           "options": [],
+                           "params": [],
+                           },
                           {"name": "ten",
                            "label": "Diez",
                            "docs": None,
                            "field_type": "numeric",
-                           "options": []},
+                           "options": [],
+                           "params": [],
+                           },
                           {'name': 'true_bool',
                            'label': 'True Bool',
                            "docs": None,
                            'field_type': 'boolean',
-                           'options': []}])
-        
+                           'options': [],
+                           "params": [],
+                           },
+                          ])
+
         variable_type_operators = all_data.get("variable_type_operators")
         self.assertEqual(variable_type_operators, expected_variable_type_operators)
 
 
-
 expected_variable_type_operators = {
-                'boolean': [
-                    {'input_type': 'none', 'label': 'Is False', 'name': 'is_false'},
-                    {'input_type': 'none', 'label': 'Is True', 'name': 'is_true'}
-                ],
-                'numeric': [
-                    {'input_type': 'none', 'label': 'Does Not Exist', 'name': 'does_not_exist'},
-                    {'input_type': 'numeric', 'label': 'Equal To', 'name': 'equal_to'},
-                    {'input_type': 'numeric', 'label': 'Greater Than', 'name': 'greater_than'},
-                    {'input_type': 'numeric', 'label': 'Greater Than Or Equal To', 'name': 'greater_than_or_equal_to'},
-                    {'input_type': 'numeric', 'label': 'Less Than', 'name': 'less_than'},
-                    {'input_type': 'numeric', 'label': 'Less Than Or Equal To', 'name': 'less_than_or_equal_to'},
-                    {'input_type': 'numeric', 'label': 'Not Equal To', 'name': 'not_equal_to'}
-                ],
-                'select': [
-                    {'input_type': 'select', 'label': 'Contains', 'name': 'contains'},
-                    {'input_type': 'select', 'label': 'Contains All', 'name': 'contains_all'},
-                    {'input_type': 'select', 'label': 'Contains Any', 'name': 'contains_any'},
-                    {'input_type': 'select', 'label': 'Does Not Contain', 'name': 'does_not_contain'}
-                ],
-                'select_multiple': [
-                    {'input_type': 'select_multiple', 'label': 'Compare State With Item (Only for Posting Rule Engine)', 'name': 'compare_state_with_item'},
-                    {'input_type': 'select_multiple', 'label': 'Contains All', 'name': 'contains_all'},
-                    {'input_type': 'select_multiple', 'label': 'Is Contained By', 'name': 'is_contained_by'},
-                    {'input_type': 'select_multiple', 'label': 'Shares At Least One Element With', 'name': 'shares_at_least_one_element_with'},
-                    {'input_type': 'select_multiple', 'label': 'Shares Exactly One Element With', 'name': 'shares_exactly_one_element_with'},
-                    {'input_type': 'select_multiple', 'label': 'Shares No Elements With', 'name': 'shares_no_elements_with'}
-                ],
-                'string': [
-                    {'input_type': 'text', 'label': 'Contains', 'name': 'contains'},
-                    {'input_type': 'text', 'label': 'Contains (case insensitive)', 'name': 'contains_case_insensitive'},
-                    {'input_type': 'text', 'label': 'Does Not Contain', 'name': 'does_not_contain'},
-                    {'input_type': 'text', 'label': 'Does not contain (case insensitive)', 'name': 'does_not_contain_case_insensitive'},
-                    {'input_type': 'text', 'label': 'Does Not End With', 'name': 'does_not_end_with'},
-                    {'input_type': 'text', 'label': 'Does Not Match Regex', 'name': 'does_not_match_regex'},
-                    {'input_type': 'text', 'label': 'Does Not Start With', 'name': 'does_not_start_with'},
-                    {'input_type': 'text', 'label': 'Ends With', 'name': 'ends_with'},
-                    {'input_type': 'text', 'label': 'Equal To', 'name': 'equal_to'},
-                    {'input_type': 'text', 'label': 'Equal To (case insensitive)', 'name': 'equal_to_case_insensitive'},
-                    {'input_type': 'none', 'label': 'Is Empty', 'name': 'is_empty'},
-                    {'input_type': 'text', 'label': 'Matches Regex', 'name': 'matches_regex'},
-                    {'input_type': 'none', 'label': 'Non Empty', 'name': 'non_empty'},
-                    {'input_type': 'text', 'label': 'Not Equal To', 'name': 'not_equal_to'},
-                    {'input_type': 'text', 'label': 'Not equal To (case insensitive)', 'name': 'not_equal_to_case_insensitive'},
-                    {'input_type': 'text', 'label': 'Starts With', 'name': 'starts_with'}
-                ]
-            }
+    'boolean': [
+        {'input_type': 'none', 'label': 'Is False', 'name': 'is_false'},
+        {'input_type': 'none', 'label': 'Is True', 'name': 'is_true'}
+    ],
+    'numeric': [
+        {'input_type': 'none', 'label': 'Does Not Exist', 'name': 'does_not_exist'},
+        {'input_type': 'numeric', 'label': 'Equal To', 'name': 'equal_to'},
+        {'input_type': 'numeric', 'label': 'Greater Than', 'name': 'greater_than'},
+        {'input_type': 'numeric', 'label': 'Greater Than Or Equal To', 'name': 'greater_than_or_equal_to'},
+        {'input_type': 'numeric', 'label': 'Less Than', 'name': 'less_than'},
+        {'input_type': 'numeric', 'label': 'Less Than Or Equal To', 'name': 'less_than_or_equal_to'},
+        {'input_type': 'numeric', 'label': 'Not Equal To', 'name': 'not_equal_to'}
+    ],
+    'select': [
+        {'input_type': 'select', 'label': 'Contains', 'name': 'contains'},
+        {'input_type': 'select', 'label': 'Contains All', 'name': 'contains_all'},
+        {'input_type': 'select', 'label': 'Contains Any', 'name': 'contains_any'},
+        {'input_type': 'select', 'label': 'Does Not Contain', 'name': 'does_not_contain'}
+    ],
+    'select_multiple': [
+        {'input_type': 'select_multiple', 'label': 'Compare State With Item (Only for Posting Rule Engine)',
+         'name': 'compare_state_with_item'},
+        {'input_type': 'select_multiple', 'label': 'Contains All', 'name': 'contains_all'},
+        {'input_type': 'select_multiple', 'label': 'Is Contained By', 'name': 'is_contained_by'},
+        {'input_type': 'select_multiple', 'label': 'Shares At Least One Element With',
+         'name': 'shares_at_least_one_element_with'},
+        {'input_type': 'select_multiple', 'label': 'Shares Exactly One Element With',
+         'name': 'shares_exactly_one_element_with'},
+        {'input_type': 'select_multiple', 'label': 'Shares No Elements With', 'name': 'shares_no_elements_with'}
+    ],
+    'string': [
+        {'input_type': 'text', 'label': 'Contains', 'name': 'contains'},
+        {'input_type': 'text', 'label': 'Contains (case insensitive)', 'name': 'contains_case_insensitive'},
+        {'input_type': 'text', 'label': 'Does Not Contain', 'name': 'does_not_contain'},
+        {'input_type': 'text', 'label': 'Does not contain (case insensitive)',
+         'name': 'does_not_contain_case_insensitive'},
+        {'input_type': 'text', 'label': 'Does Not End With', 'name': 'does_not_end_with'},
+        {'input_type': 'text', 'label': 'Does Not Match Regex', 'name': 'does_not_match_regex'},
+        {'input_type': 'text', 'label': 'Does Not Start With', 'name': 'does_not_start_with'},
+        {'input_type': 'text', 'label': 'Ends With', 'name': 'ends_with'},
+        {'input_type': 'text', 'label': 'Equal To', 'name': 'equal_to'},
+        {'input_type': 'text', 'label': 'Equal To (case insensitive)', 'name': 'equal_to_case_insensitive'},
+        {'input_type': 'none', 'label': 'Is Empty', 'name': 'is_empty'},
+        {'input_type': 'text', 'label': 'Matches Regex', 'name': 'matches_regex'},
+        {'input_type': 'none', 'label': 'Non Empty', 'name': 'non_empty'},
+        {'input_type': 'text', 'label': 'Not Equal To', 'name': 'not_equal_to'},
+        {'input_type': 'text', 'label': 'Not equal To (case insensitive)', 'name': 'not_equal_to_case_insensitive'},
+        {'input_type': 'text', 'label': 'Starts With', 'name': 'starts_with'}
+    ]
+}
--- tests/test_variables_class.py
+++ tests/test_variables_class.py
@@ -1,4 +1,6 @@
-from business_rules.variables import BaseVariables, rule_variable
+from business_rules.engine import check_condition
+from business_rules.fields import FIELD_NUMERIC
+from business_rules.variables import BaseVariables, rule_variable, boolean_rule_variable
 from business_rules.operators import StringType
 from unittest import TestCase
 
@@ -22,14 +24,30 @@ class VariablesClassTests(TestCase):
             def non_rule(self):
                 return "baz"
 
+            @boolean_rule_variable(
+                params=[{"fieldType": FIELD_NUMERIC, "name": "x", "label": "X"},
+                        {"fieldType": FIELD_NUMERIC, "name": "y", "label": "Y"}]
+            )
+            def compare(self, x, y):
+                return x > y
+
         vars = SomeVariables.get_all_variables()
-        self.assertEqual(len(vars), 1)
-        self.assertEqual(vars[0]['name'], 'this_is_rule_1')
-        self.assertEqual(vars[0]['label'], 'This Is Rule 1')
-        self.assertEqual(vars[0]['field_type'], 'string')
-        self.assertEqual(vars[0]['docs'], 'some docs')
-        self.assertEqual(vars[0]['options'], [])
+        self.assertEqual(len(vars), 2)
+        self.assertEqual(vars[1]['name'], 'this_is_rule_1')
+        self.assertEqual(vars[1]['label'], 'This Is Rule 1')
+        self.assertEqual(vars[1]['field_type'], 'string')
+        self.assertEqual(vars[1]['docs'], 'some docs')
+        self.assertEqual(vars[1]['options'], [])
 
         # should work on an instance of the class too
-        self.assertEqual(len(SomeVariables().get_all_variables()), 1)
+        self.assertEqual(len(SomeVariables().get_all_variables()), 2)
+
+        condition = {
+            "name": "compare",
+            "operator": "is_true",
+            "value": '',
+            "params": {"x": 9, "y": 6},
+        }
+        condition_result = check_condition(condition, SomeVariables())
+        self.assertTrue(condition_result)
 
